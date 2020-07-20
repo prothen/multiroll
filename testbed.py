bdc3b90ed25dee747b4807f66354e199494d7266
Philipp Rothenhäusler <phirot@kth.se>
2020-07-20 20:45:41 +0200

    Update vertices_plot from graph.py to testbed.py

--- testbed.py
+++ testbed.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
             show_agents=True, 
             show_predictions=False, 
             show_observations=False)
-    g.show_vertices() 
+    g.show_vertices(env_renderer) 
     input('press to close')
     #g = MyGraph()
     # select random agent initial position (Use GridTransitionMap)
