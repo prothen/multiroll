054267c0057eca52f05b9a6edae5b64f32a051ca
Philipp Rothenhäusler <phirot@kth.se>
2020-07-20 23:45:23 +0200

    Clean up testbed and plotting

--- testbed.py
+++ testbed.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 
-import numpy
-import pandas
-import networkx
-import matplotlib
 
 import copy
 import enum
 import time
-import itertools
-import collections
-# from multiprocessing import Process, Value
-
 import graph
+import numpy
+import pandas
+import networkx
+import itertools
+import matplotlib
+import collections
 
-# from flatland.core.grid.grid4 import Grid4TransitionsEnum
-# from flatland.core.grid.grid4_utils import get_new_position
 
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_generators import sparse_rail_generator
@@ -37,39 +33,37 @@ class EmptyObservation(ObservationBuilder):
         pass
 
     def get(self, handle: int = 0):
-        #print(self.env.agents[0].status)
-        print('custom observation')
-        return 'lol'
+        return np.ones((self.env.num_agents()))
 
 numpy.random.seed(1)
 
-#params_malfunction = MalfunctionParameters(
-#        malfunction_rate=30,  # Rate of malfunction occurence
-#        min_duration=3,  # Minimal duration of malfunction
-#        max_duration=20  # Max duration of malfunction
-#        )
-#
-ratios = {1.: 0.25,  # Fast passenger train
-          1. / 2.: 0.25,  # Fast freight train
-          1. / 3.: 0.25,  # Slow commuter train
-          1. / 4.: 0.25}  # Slow freight train
+params_malfunction = MalfunctionParameters(
+        malfunction_rate=30, 
+        min_duration=3,  
+        max_duration=20  
+        )
+
+ratios = {1.: 0.25,  
+          1. / 2.: 0.25,
+          1. / 3.: 0.25,
+          1. / 4.: 0.25} 
 gen_schedule = sparse_schedule_generator(ratios)
 
 env = RailEnv(
         width=30,
         height=30,
         rail_generator=sparse_rail_generator(
-            max_num_cities=2,  # Number of cities in map (where train stations are)
-            seed=14,  # Random seed
+            max_num_cities=2, 
+            seed=14,
             grid_mode=False,
             max_rails_between_cities=2,
             max_rails_in_city=2,
             ),
         schedule_generator=gen_schedule,
         number_of_agents=1,
-#        malfunction_generator_and_process_data=malfunction_from_params(
-#            params_malfunction),  # Malfunction data generator
-        obs_builder_object=GlobalObsForRailEnv(),  #EmptyObservation(), #GlobalObsForRailEnv(),
+        malfunction_generator_and_process_data=malfunction_from_params(
+            params_malfunction),
+        obs_builder_object=GlobalObsForRailEnv(),
         remove_agents_at_target=True,
         record_steps=True
         )
@@ -84,21 +78,12 @@ if __name__ == "__main__":
     print('start test program')
     env.reset()
     env_renderer.reset()
-    #env_renderer.render_env(show=True, show_observations=False, show_predictions=False)
-    # import sys
-    # sys.exit(0)
     print('Start Graph')
     t0 = time.time()
     g = graph.MyGraph(env)
-    #input('wait')
-    #import sys
-    #sys.exit(0)
-    #g.initialise()
     print('Graph creation completed!\n\t--> {}s'.format(time.time()-t0))
     for step in range(500):
-        # print('Step {}'.format(step))
         env.step(dict((a,0) for a in range(env.get_num_agents())))
-        # print('Step {}'.format(step))
 
     env_renderer.render_env(
             show=True, 
@@ -108,8 +93,4 @@ if __name__ == "__main__":
     g.show_vertices(env_renderer) 
     env_renderer.gl.show()
     input('press to close')
-    #g = MyGraph()
-    # select random agent initial position (Use GridTransitionMap)
-    
-    # select available actions (decode the uint16_t code)
-    # if multiple transitions create node with coordinate
+
