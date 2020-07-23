ca40a283f6c8798535a5130409a3fcef9dd3dbd1
Philipp Rothenhäusler <phirot@kth.se>
2020-07-24 01:25:01 +0200

    Add graph.py complete sparsification

--- graph.py
+++ graph.py
@@ -19,13 +19,6 @@ Edge = collections.namedtuple('Edge', ['vertex_1', 'vertex_2', 'attributes'])
 # Stores Goal Vertex and distance in cell count
 PartialEdge = collections.namedtuple('PartialEdge', ['vertex', 'distance'])
 
-class Control(enum.IntEnum):
-    NONE = 0
-    L = 1
-    F = 2
-    R = 3
-    S = 4
-    COUNT = 5
 
 class Direction(enum.IntEnum):
     N = 0
@@ -34,11 +27,27 @@ class Direction(enum.IntEnum):
     W = 3
     COUNT = 4
 
+
+class Control(enum.IntEnum):
+    NONE = 0
+    L = 1
+    F = 2
+    R = 3
+    S = 4
+    COUNT = 5
+
+
+Tests = [[Control.L, -1],
+         [Control.F, 0],
+         [Control.R, 1]]
+
+
 Transition2Color = dict()
 Transition2Color[Direction.N] = 'r'
 Transition2Color[Direction.E] = 'r'
 Transition2Color[Direction.S] = 'r'
 Transition2Color[Direction.W] = 'r'
+
 
 Direction2Target = dict()
 Direction2Target[Direction.N] = [-1, 0]
@@ -46,32 +55,33 @@ Direction2Target[Direction.E] = [0, 1]
 Direction2Target[Direction.S] = [1, 0]
 Direction2Target[Direction.W] = [1, -1]
 
+
 Dynamics = dict()
 Dynamics[Direction.N] = (lambda state: State(state.r-1, state.c, Direction.N))
 Dynamics[Direction.E] = (lambda state: State(state.r, state.c+1, Direction.E))
 Dynamics[Direction.S] = (lambda state: State(state.r+1, state.c, Direction.S))
 Dynamics[Direction.W] = (lambda state: State(state.r, state.c-1, Direction.W))
 
-Tests = [[Control.L, -1], 
-         [Control.F, 0],
-         [Control.R, 1]]
+
+
 
 FlipControlDirection = dict()
-FlipControlDirection[Direction.N] = (lambda control: ControlDirection(control.control, Direction.S))
-FlipControlDirection[Direction.E] = (lambda control: ControlDirection(control.control, Direction.W))
-FlipControlDirection[Direction.S] = (lambda control: ControlDirection(control.control, Direction.N))
-FlipControlDirection[Direction.W] = (lambda control: ControlDirection(control.control, Direction.E))
+FlipControlDirection[Direction.N] = (lambda control: ControlDirection(Control.NONE, Direction.S))
+FlipControlDirection[Direction.E] = (lambda control: ControlDirection(Control.NONE, Direction.W))
+FlipControlDirection[Direction.S] = (lambda control: ControlDirection(Control.NONE, Direction.N))
+FlipControlDirection[Direction.W] = (lambda control: ControlDirection(Control.NONE, Direction.E))
 
 class MyGraph(object):
     """ """
-    def __init__(self, env):
+    def __init__(self, env, debug=False):
+        self._verbose = debug
         self.graph = networkx.Graph()
 
         self.n_vertices = 0  # Value('i', 0)
 
         self.vertices = dict()
         self.states = dict()
-        
+
         self.env = env
         self.grid = env.rail.grid
         railway = numpy.nonzero(self.grid)
@@ -130,23 +140,17 @@ class MyGraph(object):
             valid_directions = _valid_directions(all_control_bits)
             vertex_directions = _vertex_directions(all_control_bits, valid_directions) 
             controls = _controls(all_control_bits, valid_directions)
+            
+            #print('R{:02d} - C{:02d}: {}'.format(r, c, all_control_bits)) \
+            #        if self._verbose else None
             for d, controls in zip(valid_directions,controls):
                 state = State(r, c, d)
-                updated_controls = list()
-                updated_controls += controls
-                print('\nStart:')
-                print(updated_controls)
+                updated_controls = controls
                 for i, control in enumerate(controls):
                     state_test = Dynamics[control.direction](state)
                     if not _is_railway(state_test):
-                        print('Also change the control') # --> this needs to change the direction of the vehicle as well!
-                        print('Found end of railway at {}'.format(state_test))
-                        print('--> old Control {}'.format(control))
+                        print('\t\t->DEADEND')
                         updated_controls[i] = FlipControlDirection[control.direction](control)
-                        print('--> new Control {}'.format(updated_controls[i]))
-                 
-                print(updated_controls)
-                print('END:')
                 states[state] = updated_controls
                 if d in vertex_directions:
                     vertices[state] = None
@@ -154,34 +158,38 @@ class MyGraph(object):
         def find_vertices(vertex: State):
             edges = list()
             controls = states[vertex]
-            state = vertex
             for control in controls:
-                print('Start exploring {}:'.format(state))
+                state = vertex
                 path = 0
                 controls_i = [control]
                 while len(controls_i) < 2:
                     controls_i = controls_i[0]
-                    print('{:05d}'.format(path), Direction(controls_i.direction),
-                          'from {}'.format(state))
-                    #print('--> Control:', Direction(controls_i.direction))
                     state = Dynamics[controls_i.direction](state)
-                    #print('to states \n\t{}'.format(state))
                     controls_i = states[state]
                     path += 1
                 edges.append(Edge(vertex, state, path))
-                #print('Found Vertex {} for {}'.format(state, vertex))
             return edges
-        
-        #edge_list = list()
-        #for vertex in vertices.keys():
-        #    # Return found vertex and its path length
-        #    edge_list += find_vertices(vertex)
-        ##self.report_vertices()
-        #print(edge_list)
+
+        def connect_targets(env):
+            for agent in env.agents:
+                print(agent.target_position)
+                # get r, c grid 
+                # get valid transitions
+                # explore each to next vertex
+
+        edge_list = list()
+        for vertex in vertices.keys():
+            pass
+            # Return found vertex and its path length
+            edge_list += find_vertices(vertex)
+        self.report_vertices()
+
 
     def report_vertices(self):
         for vertex in self.vertices.keys():
-            print('\tVertex: \t{}'.format(vertex))
+            pass
+            #print('\tVertex: \t{}'.format(vertex))
+            #print('\tControl: \t{}'.format(self.states[vertex]))
 
     def show_vertices(self, env_renderer):
         v = list(self.vertices.keys())[0] 
