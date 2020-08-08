fd83fbc4de27c16f0bc932f5b6179eefba501bc0
Philipp Rothenhäusler <phirot@kth.se>
2020-08-08 13:10:09 +0200

    Cleanup profiler and resources

--- generate_pstats_profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NAME="profiling_report"
-FILEPATH="./figures/$NAME"
-DESTPATH="./resources/$NAME"
-python -m cProfile -o $DESTPATH ./testbed.py --display False >/dev/null 2>&1
-gprof2dot -f pstats  $DESTPATH | dot -Tsvg -o $FILEPATH.svg
-# inkscape -z -w 2048 -h 1920 $FILEPATH.svg -e $DESTPATH.png
-eog $DESTPATH.svg
-
