d3c936262e7ec1f1fbac21cf7dfff1f9c8102278
Philipp Rothenhäusler <phirot@kth.se>
2020-08-08 12:17:51 +0200

    Update timeme to ms formatting

---
+++ timeme.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+import time
+
+S2MS = 1.e3
+
+timestamp = time.time()
+
+
+def timeme_reset():
+    global timestamp
+    timestamp = time.time()
+
+
+def timeme(message):
+    global timestamp
+    global S2MS
+    print(message, '\n\t({:3.2}ms)'.format((time.time() - timestamp)*S2MS))
+    timestamp = time.time()
+
