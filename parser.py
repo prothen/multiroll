0dc5f1d6b18e7ad46556a8fd63f770e057ff9064
Philipp Rothenhäusler <phirot@kth.se>
2020-08-08 13:08:24 +0200

    Add argparse module

---
+++ parser.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+import argparse
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--display', type=str2bool, dest='display_active', help='Enable visualisation', default=False)
