8b17a242e0ed430868d64fcbbe11d925e4e27590
Philipp Rothenhäusler <phirot@kth.se>
2020-08-09 20:17:32 +0200

    Add agent initialisation to rollout

--- multiroll/rollout.py
+++ multiroll/rollout.py
@@ -7,7 +7,7 @@ from .constants import *
 from .framework import *
 
 
-class Rollout(multiroll.simulator.Simulator):
+class Rollout(multiroll.simulator.Simulation):
     """ Rollout implementation for networkX graph. """
 
     def __init__(self, *args, **kwargs):
@@ -18,6 +18,17 @@ class Rollout(multiroll.simulator.Simulator):
         self.priority_dict = None
         # Controls indexed by agent_id and action
         self._controls = dict()
+
+        for agent in self.agents.values():
+            agent.update()
+            agent.locate()
+            path, heuristic, status = self.compute_heuristic(agent)
+
+            if status == PathStatus.INFEASIBLE:
+                agent.path_status = status
+                raise RuntimeError()
+                continue
+            agent.set_controller(path, heuristic)
 
     def update_agent_states(self):
         """ Update each agent state with most recent flatland states.
@@ -42,6 +53,10 @@ class Rollout(multiroll.simulator.Simulator):
         """
         d_M = self.prediction_horizon
         for agent in self.sim_agents.values():
+            # TODO: Test
+            if agent.status == AgentStatus.ON_PATH:
+                self._controls[agent.id] = agent.controller[agent.state]
+                continue
             self._reset_sim_agents()
             state = agent.state
 
@@ -50,11 +65,11 @@ class Rollout(multiroll.simulator.Simulator):
 
             # Costs indexed by corresponding control
             costs = dict()
-            # Heuristics indexed by controls
+            # Heuristics indexed by controls at current step
             heuristics = dict()
 
             # Get active heuristic cost
-            control_heuristic = agent.heuristic[state]
+            control_heuristic = agent.controller[state]
             cost = self.simulate_steps(d_M)
             costs[control_heuristic] = cost
 
@@ -73,8 +88,8 @@ class Rollout(multiroll.simulator.Simulator):
 
             min_control = min(costs, key=costs.get)
             if min_control != control_heuristic:
-                agent.set_base_heuristic(heuristics[min_control])
-                self._controls[agent.id] = min_control.control
+                agent.set_controller(heuristics[min_control])
+            self._controls[agent.id] = min_control.control
 
     def controls(self):
         """ Conduct rollout and return the control. """
