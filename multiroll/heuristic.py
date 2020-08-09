666d823dcd4dc845d3d67491faa45cf6e1702d26
Philipp Rothenhäusler <phirot@kth.se>
2020-08-09 17:33:19 +0200

    Update shortest path heuristic docstrings

--- multiroll/heuristic.py
+++ multiroll/heuristic.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
 """
-    Multiagent rollout implemented on flatland environment.
+    Heuristic with networkX graph shortest_path algorithm.
 
-
-    Note:
-        This module expects to be initialised with the 
-        interfacing environment
-
-            import graph
-            ...
-            graph.Graph(env)
-
-        before usage of any subsequently defined classes.
+    Author: Philipp Rothenhäusler, Stockholm 2020
 """
 
 
 import networkx
 
+
 from multiroll import *
+
 from .constants import *
 from .framework import *
 from .coordinate import *
@@ -26,25 +19,19 @@ from .edge import *
 
 
 class ShortestPath(multiroll.graph.Graph):
-    """Container for graph related actions.
+    """Shortest path algorithm based on networX.
 
-        Note:
-            Update active edges or compute shortest path.
+        Todo:
+             Use all_shortest_path computation
 
-        Note: / TODO:
-            Maybe easier to maintain two graphs (one complete and a subset 
-            dynamically updated)
-
-        Todo: 
-            - Use all_shortest_path computation 
-            - Use graph_complete mirror and 
-                - use graph_complete.copy() to reset dynamic?
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def _path_from_networkx(self, path):
         """ Convert the networkX tuples into edge_container references. """
+
         path_nodes = list()
         for idx, node in enumerate(path):
             if idx == len(path)-1:
@@ -61,10 +48,6 @@ class ShortestPath(multiroll.graph.Graph):
 
             # Path is [k_1, k_2, ..., k_M] add k_0 entry state
             heuristic.update([(node, control)])
-
-            # Find its edge_container through the edge.id
-            edge_container = self.edges[edge.container_id]
-            path_edges.append(edge_container)
 
         # TODO
         print(path)
