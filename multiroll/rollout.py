2e218ef42673afe2d49bb40f09df59c15f8f761e
Philipp Rothenhäusler <phirot@kth.se>
2020-08-09 17:39:02 +0200

    Cleanup rollout algorithm and class

--- multiroll/rollout.py
+++ multiroll/rollout.py
@@ -9,10 +9,10 @@ from .framework import *
 
 class Rollout(multiroll.simulator.Simulator):
     """ 
-        
+
         Rollout only interacts with simulator to get flatland metrics. So
         only simulator receives env argument.
-    
+
         Note:
             agent_container:
                 - should provide feed_forward control to simulator
@@ -33,75 +33,66 @@ class Rollout(multiroll.simulator.Simulator):
                 Specify desired keywords for this class explicitly
         """
         super().__init__(*args, **kwargs)
-        #self.simulator = multiroll.simulator.Simulator(*args, **kwargs)
-        # Dictionary of agents with key: agent_id and value: agent_container
-        # TODO: initialise agents from simulator
-        # self.agents = None
         # Integer for prediction horizon
-        self.prediction_horizon = None
+        self.prediction_horizon = 30
         # priority dictionary: sorted agents according to their current edge
         self.priority_dict = None
         # Controls indexed by agent_id and action
-        #self.controls = dict()
+        self._controls = dict()
 
-
-    def _rollout(self, agent_id):
-        """ Run the rollout for one agent.
-
-            Note:
-                Selects the best control based on cost
-                returned from simulator.
-
-                agent_container:
-                    - find controls other than heuristic
-                    - store heuristic dict (states -> control)
-                    - heuristic from all edges -> state to control pair
-
-                simulator:
-                    simulate_steps
-                    simulate_heuristic_steps
-
-        """
-        d_M = self.prediction_horizon
-        agent = self.agents[agent_id].state
-        state = agent.state
-        controls = self.states[state].controls
-
-        # Costs indexed by corresponding control
-        costs = dict()
-        # Heuristics indexed by controls
-        heuristics = dict()
-        control_heuristic = agent.heuristic[state]
-        cost = self.simulator.simulate_steps(d_M)
-        costs[control_heuristic] = cost
-
-        # Remove heuristic
-        controls.pop(controls.index(control_heuristic))
-        for control in controls:
-            if simulator.update_heuristic(agent_id, control):
-                cost = simulator.simulate_steps(d_M)
-            heuristics[control] = self.agents[agent_id].heuristic
-            costs[control] = cost
-
-        min_control = min(costs, key=costs.get)
-        if min_control != control_heuristic:
-            agent.update_heuristic(heuristics[min_control])
-            self.controls[agent.id] = min_control.control
-
-    def rollout(self):
+    def _rollout(self):
         """ Execute rollout simulation for each agent. 
 
             Note:
                 Returns the controls dictionary for all agents 
                 at current stage.
-        """
-        for agent in self.agents:
-            #if self.states[agent.state].priority != Priority.None:
-            # TODO: Add is_agent_at_node to agent (agent.state.type &Node)
-            #       -> if intersection -> try waiting and see whether this changes something
-            self._rollout(agent)
-            #else:
-            #    self.controls[agent.id] = agent.heuristic[agent.state].control
-        return self.controls
 
+            Todo:
+                Consider edge priority and agent.state.status
+
+            Todo:
+                Use jit or keep context local with env update inside
+        """
+        d_M = self.prediction_horizon
+        self.sim_agents.reset()
+        for agent_id in self.agents.keys():
+            self._rollout(agent_id)
+            agent = self.sim_agents[agent_id]
+            state = agent.state
+
+            # Get all available controls at state
+            controls = self.states[state].controls
+
+            # Costs indexed by corresponding control
+            costs = dict()
+            # Heuristics indexed by controls
+            heuristics = dict()
+            control_heuristic = agent.heuristic[state]
+            cost = self.simulator.simulate_steps(d_M)
+            costs[control_heuristic] = cost
+
+            # Remove heuristic
+            controls.pop(controls.index(control_heuristic))
+            for control in controls:
+                if simulator.update_heuristic(agent_id, control):
+                    cost = simulator.simulate_steps(d_M)
+                heuristics[control] = agents[agent_id].heuristic
+                costs[control] = cost
+
+            min_control = min(costs, key=costs.get)
+            if min_control != control_heuristic:
+                # sim agents know which control has which heuristic
+                agent.update_heuristic(heuristics[min_control])
+            self._controls[agent.id] = min_control.control
+
+    def controls(self):
+        """ Conduct rollout and return the control.
+
+            Note:
+                Agents are transitioned in rollout()
+
+        """
+        self._rollout()
+
+        return self._controls
 
